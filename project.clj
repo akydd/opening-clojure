f7dfc7d86cfec05106fe5cc5d80067f18534f145
Alan Kydd <kydd.alan.g@gmail.com>
2026-07-10 13:26:48 -0600

    First commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject opening-clojure "0.1.0-SNAPSHOT"
+  :main ^{:skip-aot true} opening-clojure.song
+  :jvm-opts ^:replace []
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [overtone "0.10.1"]
+                 [leipzig "0.10.0"]])
